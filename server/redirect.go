87344377f377e3a9a86e1e318be66fb2a2d30b03
Joey Sacchini <joey.sacchini264@gmail.com>
2017-07-07 17:23:45 -0400

    use req.Host

--- server/redirect.go
+++ server/redirect.go
@@ -11,6 +11,7 @@ func (srv *Server) startRedirectServer() {
 
 func redirectHandler(w http.ResponseWriter, req *http.Request) {
 	u := *req.URL
+	u.Host = req.Host
 	u.Scheme = "https"
 	w.Header().Add("Location", u.String())
 	w.WriteHeader(http.StatusPermanentRedirect)
